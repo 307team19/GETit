39d051f718413196245c1ed91931c98bfd5eaca9
Aman Wali <amanwali27@gmail.com>
2019-02-08 01:10:26 -0500

    Created my branch

--- GETit/index.js
+++ GETit/index.js
@@ -2,9 +2,15 @@
  * @format
  * @lint-ignore-every XPLATJSCOPYRIGHT1
  */
+import React from 'react'
+import {AppRegistry, View, Text} from 'react-native';
+// import App from './App';
+// import {name as appName} from './app.json';
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+const App = ()=> (
+    <View style={{flex:1}}>
+        <Text>Testing</Text>
+    </View>
+);
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent('GETit', () => App);
