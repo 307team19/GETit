1286bd84788ad429396a5f4798a191cc097ab9d5
Aman Wali <amanwali27@gmail.com>
2019-02-10 21:15:37 -0500

    Logo + Email
    Created components for displaying app logo on log-in page and also allowing user to enter their email ID

---
+++ GETit/src_Aman/Components_Aman/Button.js
@@ -0,0 +1,41 @@
+import React from 'react';
+
+import {Text, TouchableOpacity, TouchableHighlight} from 'react-native';
+
+const Button = ({propPress, children}) => {
+    const {buttonStyle, TextStyle} = styles;
+    return (
+        //<TouchableOpacity onPress={propsPress} style={buttonStyle}>
+        <TouchableOpacity onPress={propPress} style={buttonStyle}>
+            <Text style={TextStyle}>
+                {children}
+            </Text>
+        </TouchableOpacity>
+        //<TouchableHighlight>
+        //</TouchableHighlight>
+    );
+};
+
+const styles = {
+    buttonStyle:{
+        flex: 1,
+        alignSelf: 'stretch',
+        backgroundColor: '#fff',
+        borderRadius: 5,
+        borderWidth: 1,
+        borderColor: '#007aff',
+        marginLeft: 5,
+        marginRight: 5
+    },
+    TextStyle:{
+        alignSelf: 'center',
+        fontSize: 16,
+        color: '#007aff',
+        fontWeight: 'bold',
+        // marginTop: 10,
+        paddingTop: 10,
+        paddingBottom: 10
+    }
+};
+
+export { Button };
--- GETit/src_Aman/LoginPage.js
+++ GETit/src_Aman/LoginPage.js
@@ -1,11 +1,50 @@
 import React, {Component} from 'react';
-import {View, Text} from 'react-native';
+import {View, Image} from 'react-native';
+// import {CardSection} from "./Components_Aman";
+import {Card, CardSection, Input} from "./Components_Aman";
 
 class LoginPage extends Component {
+
+    state = { email: '', password: ''};
+
     render() {
         return (
             <View style={styles.containerStyle}>
-                <Text>Hello</Text>
+                <Card>
+                    <CardSection>
+                        <Image
+                            style={styles.logoStyle}
+                            source={require('./img/logo.png')}
+                            resizeMode='contain'
+                        />
+                    </CardSection>
+
+
+                    <CardSection>
+                        <Input
+                            label="Email ID:"
+                            value={this.state.email}
+                            onChangeText={email => this.setState({email})}
+                            placeholder="john@purdue.edu"
+                        />
+                    </CardSection>
+
+
+                    <CardSection>
+                    </CardSection>
+
+
+                    <CardSection>
+                    </CardSection>
+
+
+                    <CardSection>
+                    </CardSection>
+
+
+                    <CardSection>
+                    </CardSection>
+                </Card>
             </View>
         );
     }
@@ -13,8 +52,15 @@ class LoginPage extends Component {
 
 const styles = {
     containerStyle:{
+        paddingTop:20,
         flex:1,
         backgroundColor:'white'
+    },
+    logoStyle:{
+        height:160,
+        width: 150,
+        alignItems:'center',
+        flex:1
     }
 }
 
