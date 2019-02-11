14764ba9c5493efeaaf2eab14ec41a79848acfec
Shiv Paul <shivapaul98@gmail.com>
2019-02-10 20:14:46 -0500

    Setup PaperProvider in view hierarchy

--- GETit/index.js
+++ GETit/index.js
@@ -2,15 +2,19 @@ import React from 'react';
 import {View, AppRegistry, Text} from "react-native";
 import {name as appName} from './app.json';
 import Header from './src/components_shiv/Header'
+import { Provider as PaperProvider } from 'react-native-paper';
 
 //functional component which returns some JSX for viewing
 const App = () => {
 
 	//creating headerText and setting its value + adding the AlbumList
 	return (
-		<View>
-			<Header headerText={'GETit Sign Up Page'}/>
-		</View>
+
+		<PaperProvider>
+			<View>
+				<Header headerText={'GETit Sign Up Page'}/>
+			</View>
+		</PaperProvider>
 	);
 };
 
