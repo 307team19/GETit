415a6a2d2c609081bc996fa874179686355f67b1
Shiv Paul <shivapaul98@gmail.com>
2019-02-10 19:48:55 -0500

    Stylized header component

--- GETit/src/components_shiv/Header.js
+++ GETit/src/components_shiv/Header.js
@@ -7,15 +7,16 @@ import {Text, View} from 'react-native';
  * Gets headerText and returns a view with header
  * @param headerText, text of the header
  * @returns view of header
+ */
 const Header = (props) => {   //getting props from the function
 
 	//getting the different designs from the main variable
 	//we'll use these in html to set designs
-	const {viewStyle} = styles;
+	const {textStyle, viewStyle} = styles;
 
 	return (
 		<View style={viewStyle}>
-			<Text>{props.headerText}</Text>
+			<Text style={textStyle}>{props.headerText}</Text>
 		</View>
 	);
 };
@@ -23,10 +24,29 @@ const Header = (props) => {   //getting props from the function
 //designs for different components
 const styles = {
 
+	//text design
+	textStyle: {
+
+		fontSize: 20
+	},
+
 	//view design
 	viewStyle: {
 
-		backgroundColor: '#f8f8f8'
+		backgroundColor: '#f8f8f8',
+		justifyContent: 'center',
+		alignItems: 'center',
+		paddingTop: 23,
+		paddingBottom: 7,
+
+		//ios shadow
+		shadowOffset: {width: 0, height: 2},
+		shadowColor: '#000',
+		shadowOpacity: 0.3,
+		shadowRadius: 8,
+
+		//android shadow
+		elevation: 3,
 	}
 };
 
