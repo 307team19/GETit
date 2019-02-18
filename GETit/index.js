f77a4ceb148ab7c25161c50d814645a13d32728e
Shiv Paul <shivapaul98@gmail.com>
2019-02-17 22:38:43 -0500

    Removed testing code for Password Reset

--- GETit/index.js
+++ GETit/index.js
@@ -1,6 +1,5 @@
 import App from './src/components/App'
 import {AppRegistry} from "react-native";
-import {name as appName} from './app'
-import PasswordReset from './src/components/PasswordReset'
+import {name as appName} from './app';
 
-AppRegistry.registerComponent(appName, () => PasswordReset);
+AppRegistry.registerComponent(appName, () => App);
