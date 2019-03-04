136fcf99c40a8ddf836f181cea082b7a1f8e7c67
Shiv Paul <shivapaul98@gmail.com>
2019-03-03 20:32:08 -0500

    Set up screen for request history

---
+++ GETit/src/components/RequestHistory.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import {Text, View} from 'react-native';
+
+
+class RequestHistory extends Component {
+
+	render() {
+		return (
+			<View style={{flex: 1}}>
+				<Text>Requests History</Text>
+			</View>
+		);
+	}
+}
+
+export default RequestHistory;
