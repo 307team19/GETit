60cb28b9d25bffd0bb45fb85ee23ee9f422f627d
Shiv Paul <shivapaul98@gmail.com>
2019-03-03 21:19:54 -0500

    RequestHistory temporarily completed

--- GETit/src/components/RequestComponent.js
+++ GETit/src/components/RequestComponent.js
@@ -1,17 +1,31 @@
 import React from 'react';
 import { Text, View } from 'react-native';
+import { Card } from './common/Card'
+import { CardSection } from './common/CardSection'
 
 const RequestComponent = (props) => {   //getting props from the function
 
 	const request = props.request;
 
+	//TODO: Temporary design which sucks, need to change but it works
 	return (
-		<View>
-			<Text style={styles.textD}>{request.item}</Text>
-			<Text style={styles.textD}>{request.price}</Text>
-			<Text style={styles.textD}>{request.descr}</Text>
-			<Text style={styles.textD}>{request.addr}</Text>
-		</View>
+		<Card>
+			<View>
+				<CardSection>
+					<Text style={styles.textD}>ITEM: </Text>
+					<Text style={styles.textD}>{request.item}</Text>
+					<Text style={styles.textD}>PRICE: </Text>
+					<Text style={styles.textD}>{request.price}</Text>
+					<Text style={styles.textD}>$</Text>
+				</CardSection>
+				<Text style={styles.textD}>DESCRIPTION: </Text>
+				<Text style={styles.textD}>{request.descr}</Text>
+				<View style = {{height: 10}}/>
+				<Text style={styles.textD}>ADDRESS: </Text>
+				<Text style={styles.textD}>{request.addr}</Text>
+				<View style = {{height: 10}}/>
+			</View>
+		</Card>
 	);
 };
 
