fa48f4f77233f206c3647cb34fa062f1855f788e
Shiv Paul <shivapaul98@gmail.com>
2019-03-03 21:21:12 -0500

    Restored Login screen to replace RequestHistory

--- GETit/index.js
+++ GETit/index.js
@@ -1,6 +1,5 @@
 import App from './src/components/App'
 import {AppRegistry} from "react-native";
 import {name as appName} from './app';
-import RequestHistory from './src/components/RequestHistory'
 
-AppRegistry.registerComponent(appName, () => RequestHistory);
+AppRegistry.registerComponent(appName, () => App);
