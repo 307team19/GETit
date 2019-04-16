daa006a444a1152342374f3e5f0aaa6a725aaf6f
Aman Wali <amanwali27@gmail.com>
2019-04-16 04:08:47 -0400

    Hides Verify button unless order has been accepted

--- GETit/src/components/Requests.js
+++ GETit/src/components/Requests.js
@@ -172,6 +172,20 @@ class Requests extends Component {
         }
     };
 
+    verifyView = ({item}) => {
+        if(item.acceptedBy!=""){
+            return (
+                <TouchableOpacity
+                    style={styles.buttonStyle}
+                    onPress={() => {
+                        this.props.navigation.navigate('verify', {item: item});
+                    }}>
+                    <Text style={styles.textStyle}>Verify Order</Text>
+                </TouchableOpacity>
+            );
+        }
+    };
+
 
     renderItem = ({item}) => (
 
@@ -221,13 +235,7 @@ class Requests extends Component {
                 </View>
                 
                 <View>
-                    <TouchableOpacity
-                        style={styles.buttonStyle}
-                        onPress={() => {
-                            this.props.navigation.navigate('verify', {item: item});
-                        }}>
-                        <Text style={styles.textStyle}>Verify Order</Text>
-                    </TouchableOpacity>
+                    {this.verifyView({item})}
                 </View>
             </View>
         </Card>
