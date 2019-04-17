6725096f4aad73c6f7feeb6030975be173f7529a
kartikmittal33 <mittal38@purdue.edu>
2019-04-17 02:38:21 -0400

    app running after installing react-native-camera

--- GETit/android/app/src/main/java/com/getit/MainApplication.java
+++ GETit/android/app/src/main/java/com/getit/MainApplication.java
@@ -3,6 +3,7 @@ package com.getit;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import org.reactnative.camera.RNCameraPackage;
 import com.dieam.reactnativepushnotification.ReactNativePushNotificationPackage;
 import com.github.douglasjunior.reactNativeGetLocation.ReactNativeGetLocationPackage;
 import com.reactnativecommunity.webview.RNCWebViewPackage;
@@ -31,6 +32,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNCameraPackage(),
             new ReactNativePushNotificationPackage(),
             new ReactNativeGetLocationPackage(),
             new RNCWebViewPackage(),
